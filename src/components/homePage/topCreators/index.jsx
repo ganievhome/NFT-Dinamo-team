1a4d17d69f873e3ded45bf2a112a38d3f8e8c93b
Shekhri <khamidovashakhrezada3868@gmail.com>
2024-04-21 13:17:06 +0500

    home- top creators

--- src/components/homePage/topCreators/index.jsx
+++ src/components/homePage/topCreators/index.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from './style.module.css'
+import axios from "axios";
+
 
 function TopCreators() {
-   return(
+   const [data, setData] = useState([])
+
+   useEffect(() => {
+      axios.get(`http://localhost:8000/TopCreators`)
+         .then((data) => setData(data))
+
+   }, [])
+
+
+   return (
       <>
-         <h1>TopCreators</h1>
+         <div className={styles.TopCreators}>
+            <div className={styles.container}>
+               <div className={styles.top}>
+                  <div className={styles.top__left}>
+                     <h2 className={styles.h2}>
+                        Top creators
+                     </h2>
+                     <p className={styles.p}>
+                        Checkout Top Rated Creators on the NFT Marketplace
+                     </p>
+                  </div>
+                  <Link className={styles.button} to={'/ranking'}>View Rankings</Link>
+               </div>
+               <div className={styles.card}>
+                  {data?.data?.map((elem, index) =>
+                     <div className={styles.box} key={index}>
+                        <div className={styles.box__top}>
+                           <div className={styles.id}>{elem.id}</div>
+                           <img className={styles.box__img} src={elem.creatorImage} alt="creator image" />
+                        </div>
+                        <h3 className={styles.name}>{elem.creatorTitle}</h3>
+                        <div className={styles.box__bottom}>
+                            <p className={styles.box__p}>Total Sales:</p>
+                            <span className={styles.price}>{elem.price}</span>
+                        </div>
+                     </div>
+                  )}
+               </div>
+            </div>
+         </div>
       </>
    )
 }
